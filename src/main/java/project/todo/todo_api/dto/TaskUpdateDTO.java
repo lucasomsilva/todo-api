1ec63e1126a21b37cc36ac6010f602f15b5e1d0e
lucasomsilva <lucas.msilva@sptech.school>
2025-08-16 00:11:25 -0300

    feat: Adicionando CRUD inicial da API

--- src/main/java/project/todo/todo_api/dto/TaskUpdateDTO.java
+++ src/main/java/project/todo/todo_api/dto/TaskUpdateDTO.java
@@ -1,18 +1,25 @@
 package project.todo.todo_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import project.todo.todo_api.enums.Status;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TodoUpdateDTO {
+public class TaskUpdateDTO {
+
+    @NotBlank
     private String titulo;
+
+    @NotBlank
     private String descricao;
+
+    @NotBlank
     private Status status;
-    private LocalDate updatedAt;
+    private LocalDateTime updatedAt;
 }
