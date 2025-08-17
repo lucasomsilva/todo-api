716ac1db4d62b6d0896c082cdef76b7dd162a345
lucasomsilva <lucas.msilva@sptech.school>
2025-08-17 19:59:27 -0300

    feat: Tentativa de Deploy do servidor de Banco com Flyway

--- src/main/resources/db/migration/V1__Create_task_table.sql
+++ src/main/resources/db/migration/V1__Create_task_table.sql
@@ -1,6 +1,3 @@
-IF
-OBJECT_ID('task', 'U') IS NULL
-BEGIN
 CREATE TABLE task (
     id         INT IDENTITY(1,1) PRIMARY KEY,
     titulo     VARCHAR(50) NOT NULL,
@@ -9,4 +6,3 @@ CREATE TABLE task (
     created_at DATETIME2 DEFAULT SYSUTCDATETIME(),
     updated_at DATETIME2 NULL
 )
-END
